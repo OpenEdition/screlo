594d69ce66a9974c989346fea17ecaee92256783
brrd <brrd@users.noreply.github.com>
2015-06-08 10:54:58 +0200

    Fix Test 1 (2e tentative...)

--- dist/screlo-update.js
+++ dist/screlo-update.js
@@ -1,7 +1,7 @@
 /*
     Screlo - Update script
 */
-var latest = "15.6.0",
+var latest = "15.6.2",
     current = document.body.getAttribute('data-screlo-version');
 if (current && current != latest) {
     document.body.className += " screlo-update";
